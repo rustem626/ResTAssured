34b6ef295ecbf4e60793ef376aece37f52c2e162
RustamRoz <rustamrozik626@gmail.com>
2023-10-26 21:35:00 +0300

    RESTASSURE_extract

---
+++ src/test/java/ZippoTestExtract.java
@@ -0,0 +1,43 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ZippoTestExtract {
+/*
+    REST Assured
+    Build Status Maven Central
+
+    Testing and validation of REST services in Java is harder than in dynamic languages such as Ruby and Groovy.
+    REST Assured brings the simplicity of using these languages into the Java domain.
+
+
+            News
+2023-09-08: REST Assured 5.3.2 is released with several bug fixes, better Spring MockMvc Support,
+ better support for newer versions of Spring Security etc. See change log for more details.
+
+            2023-06-16: REST Assured 5.3.1 is released with several bug fixes and dependency upgrades. See change log for more details.
+            2022-11-18: REST Assured 5.3.0 is released. It adds CSRF header support and a much improved CSRF support in general.
+    It also brings the Spring MockMvc Module up-to-date with changes in Spring Framework 6 and Spring Boot 3,
+    as well as various other bug fixes and improvements. See changelog for more details.
+
+*/
+
+    @Test //negative Test
+    public void extractingJsonPath () {
+        String countryName =
+                given()
+                        .when()
+                        .get("http://api.zippopotam.us/us/90210") // http hok ise baseUri baş tarafına gelir.
+                        .then()
+                        .extract().path("country")
+                ;
+        System.out.println("countryName = " + countryName);
+        Assert.assertEquals(countryName,"Canada");
+    }
+
+}
+
+
+
+
